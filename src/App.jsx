b305cdbe569cecdf2fffd997f5482a4154d1a8fd
massdo <dorian_massoulier@hotmail.com>
2021-01-12 23:04:26 +0100

    Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+// GlobalStyle
+import GlobalStyle from "./components/GlobalStyle";
+// Import pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,31 +1,64 @@
 import home1 from "../img/home1.png";
+// Style
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <StyledAbout>
+      <StyledDescription>
         <div className="title">
-          <div className="hide">
+          <StyledHide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </StyledHide>
+          <StyledHide>
             <h2>
               your <span>dream</span>
             </h2>
-          </div>
-          <div className="hide">
+          </StyledHide>
+          <StyledHide>
             <h2>come true</h2>
-          </div>
+          </StyledHide>
         </div>
         <p>
           Contact us for any ideas that you have . We are professinals here to
           help you
         </p>
         <button>Contact Us</button>
-      </div>
-      <img src={home1} alt="guy with a camera" />
-    </div>
+      </StyledDescription>
+      <StyledImage>
+        <img src={home1} alt="guy with a camera" />
+      </StyledImage>
+    </StyledAbout>
   );
 };
+
+// Styled components
+const StyledAbout = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+const StyledDescription = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+const StyledImage = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: contain;
+  }
+`;
+const StyledHide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
