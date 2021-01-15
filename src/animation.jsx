f6b0866f70b8106278899b6593dae875500cdecd
massdo <dorian_massoulier@hotmail.com>
2021-01-15 23:54:20 +0100

    Add animations for aboutSection

--- src/animation.jsx
+++ src/animation.jsx
@@ -1,16 +1,58 @@
 export const pageAnimation = {
   hidden: {
     opacity: 0,
-    y: 300,
+    x: -300,
   },
   show: {
     opacity: 1,
-    y: 0,
-    transition: { duration: 1 },
+    x: 0,
+    transition: {
+      duration: 0.8,
+      //   when: "beforeChildren",
+      staggerChildren: 0.2,
+    },
   },
   exit: {
     opacity: 0,
     y: 300,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+// child of pageAnimation
+export const titleAnim = {
+  hidden: {
+    y: 200,
+  },
+  show: {
+    y: 0,
+    transition: { duration: 2, ease: "easeOut" },
+  },
+};
+// child pageAnimation
+export const fade = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+    rotate: 30,
+  },
+  show: {
+    x: 0,
+    opacity: 1,
+    rotate: 0,
     transition: { duration: 1 },
+  },
+};
+// child pageAnimation
+export const photoAnim = {
+  hidden: {
+    scale: 0.3,
+    opacity: 0.2,
+  },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: { duration: 1.5, ease: "easeOut" },
   },
 };
--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,6 +3,7 @@ import home1 from "../img/home1.png";
 import * as S from "../styles.jsx";
 // Framer Motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 
 const AboutSection = () => {
   return (
@@ -10,25 +11,25 @@ const AboutSection = () => {
       <S.Description>
         <motion.div>
           <S.Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </S.Hide>
           <S.Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dream</span>
             </motion.h2>
           </S.Hide>
           <S.Hide>
-            <motion.h2>come true</motion.h2>
+            <motion.h2 variants={titleAnim}>come true</motion.h2>
           </S.Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any ideas that you have . We are professinals here to
           help you
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </S.Description>
       <S.Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </S.Image>
     </S.About>
   );
