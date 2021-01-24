0f1b96942bb16ff1c607fe4ea737e702f333c86b
massdo <dorian_massoulier@hotmail.com>
2021-01-24 19:18:47 +0100

    delete images

--- src/movieState.jsx
+++ src/movieState.jsx
@@ -3,7 +3,6 @@ import deadEnd from "./img/neons/pexels-aleksandar-pasaric-1820770.jpg";
 import love from "./img/neons/pexels-joshua-teichroew-1476635.jpg";
 import veltex from "./img/neons/pexels-brett-sayles-1264939.jpg";
 import orient from "./img/neons/pexels-ba-phi-1710995.jpg";
-import theracer2 from "./img/the-racer2.jpg";
 import neonGirl from "./img/pexels-alex-powell-1769340.jpg";
 
 export const MovieState = () => {
